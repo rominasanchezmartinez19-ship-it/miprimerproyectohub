92793fe32050ea96a2a457198a644aedfab74218
Romi <rominasanchezmartinez19@gmail.com>
2026-02-09 22:44:35 -0600

    Mi primer codigo

---
+++ except value.py
@@ -0,0 +1,9 @@
+#try except value permite la validacion de entrada de datos
+while True:
+    try:
+        alumno=input("Dame el nombre del alumno:")
+        cal1=float(input("Dame la primera calif:"))
+        cal2=float(input("Dame la segunda calif:"))
+        break
+    except ValueError:
+                   print("Error: Debes ingresar numeros validos")
